d4e8ebefab0bd26014c26a0564bc9ac6efe4cab4
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2024-01-19 19:02:15 +0330

    first2

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("We are testing Java ......");
+        
     }
 }
---
+++ Test1.java
@@ -0,0 +1,8 @@
+
+    public class Test1 {
+        public static void main(String[] args) {
+            System.out.println("3.5 * 4 / 2 – 2.5 is ");
+            System.out.println(3.5 * 4 / 2 - 2.5);
+        }
+    }
+
